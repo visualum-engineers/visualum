54e5f6e31c3bdec516be36b5ffe4e68f51c1fdcf
Derek Widmer <dwidmer99@gmail.com>
2021-08-11 15:42:07 -0400

    Added container to footer to make it more compact

--- frontend/src/app/components/footer/footer.js
+++ frontend/src/app/components/footer/footer.js
@@ -7,42 +7,42 @@ let time = d.getFullYear()
 export default class Footer extends Component {
     render() {
         return (
-        <footer>
-            <div id="footer" className="d-flex flex-wrap py-2">
-                <div className="col-sm-4 col-12 text-center py-3">
-                    <h5>Visualum</h5>
-                    <div className="d-flex flex-column align-items-center">
-                        <a className="foot-link" href="#">For Teachers</a>
-                        <a className="foot-link" href="#">For Students</a>
-                        <a className="foot-link" href="#">For Enterprise</a>
+            <footer className="container">
+                <div id="footer" className="d-flex flex-wrap py-2">
+                    <div className="col-sm-4 col-12 text-center py-3">
+                        <h5>Visualum</h5>
+                        <div className="d-flex flex-column align-items-center">
+                            <a className="foot-link" href="#">For Teachers</a>
+                            <a className="foot-link" href="#">For Students</a>
+                            <a className="foot-link" href="#">For Enterprise</a>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4 col-12 text-center py-3">
+                        <h5>Social</h5>
+                        <div className="d-flex flex-column align-items-center">
+                            <a className="foot-link" href="#">LinkedIn</a>
+                            <a className="foot-link" href="#">Instagram</a>
+                            <a className="foot-link" href="#">Facebook</a>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4 col-12 text-center py-3">
+                        <h5>About</h5>
+                        <div className="d-flex flex-column align-items-center">
+                            <a className="foot-link" href="#">Terms</a>
+                            <a className="foot-link" href="#">Contact Us</a>
+                            <a className="foot-link" href="#">Meet the Team</a>
+                        </div>
                     </div>
                 </div>
 
-                <div className="col-sm-4 col-12 text-center py-3">
-                    <h5>Social</h5>
-                    <div className="d-flex flex-column align-items-center">
-                        <a className="foot-link" href="#">LinkedIn</a>
-                        <a className="foot-link" href="#">Instagram</a>
-                        <a className="foot-link" href="#">Facebook</a>
+                <div id="copyright" className="row">
+                    <div className="text-center">
+                        <p><i className="fa fa-copyright"></i> {time} Visualum All rights reserved </p>
                     </div>
                 </div>
-
-                <div className="col-sm-4 col-12 text-center py-3">
-                    <h5>About</h5>
-                    <div className="d-flex flex-column align-items-center">
-                        <a className="foot-link" href="#">Terms</a>
-                        <a className="foot-link" href="#">Contact Us</a>
-                        <a className="foot-link" href="#">Meet the Team</a>
-                    </div>
-                </div>
-            </div>
-
-            <div id="copyright" className="row">
-                <div className="text-center">
-                    <p><i className="fa fa-copyright"></i> {time} Visualum All rights reserved </p>
-                </div>
-            </div>
-        </footer>
+            </footer>
 
         )
     }
