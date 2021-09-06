1bda7b723db89e776ee9ed7a1c24a1c84b6b020c
Arky Asmal <arkyasmal@gmail.com>
2021-09-06 12:19:29 -0400

    "match activity beta ver complete. No full touch functionality, but full mouse functionality"

--- frontend/src/app/components/gameTypes/MatchActivity/GridTiles.js
+++ frontend/src/app/components/gameTypes/MatchActivity/GridTiles.js
@@ -1,19 +1,16 @@
-import React, { useState, useEffect} from 'react'
-import Draggable from 'react-draggable';
+import React, {useState, useEffect} from 'react'
+import DraggableCore from 'react-draggable';
 
-const GridTiles = ({id, content, index, onDragEnd, nodeRef, size})=>{
-    const [tileBounds, setTileBounds] = useState(null)
-
+const GridTiles = ({id, content, index, onStop, onDrag, onStart, nodeRef, gridSize})=>{
+    //this will set bounds so element is limited to gridLayout area
+    const [bounds, setBounds] = useState(null)
     //gets coordinates of all four corners of the grid layout that all tiles will reside in
-    const gridRect = size.rect
+    const gridRect = gridSize.rect
 
     //gets four corners of tile element
     const tileRect = useTilePosition()
 
-    //stores difference between mouse x-coordinate, and left-corner of element
-    let xOffset
-
-    // Hook for resizing of Tiles
+    // Hook for resizing of window changing tile position
     function useTilePosition() {
         const [tilePosition, setTilePosition] = useState({rect: undefined});
         useEffect(() => {
@@ -27,46 +24,42 @@ const GridTiles = ({id, content, index, onDragEnd, nodeRef, size})=>{
         // Add event listener
         window.addEventListener("resize", handleResize);
         handleResize();
+
         // Remove event listener on cleanup
         return () => window.removeEventListener("resize", handleResize);
         }, []); // Empty array ensures that effect is only run on mount
         return tilePosition;
     }
-
-    const onStart = (e) =>{
-        //updates difference between mouse x-coordinate and left-corner of element
-        xOffset = Math.floor(tileRect.rect.right + 5) - e.clientX
-    }
-
-    const onDrag =(e, data) =>{
-        //prevents element from being dragged out of bounds
-        if(Math.abs(e.clientX + xOffset) >= gridRect.right){
-            setTileBounds({
-                right :gridRect.right - tileRect.rect.right
-            })
-        }
-    }
-
-    const onStop = (e, data) =>{
-        
-    }
-    
+    const onMouseDown = () =>{
+        //will update draggable bounds of element
+        setBounds({
+            top: gridRect.top - tileRect.rect.top+100,
+            bottom: gridRect.bottom - tileRect.rect.bottom,
+            left:  -tileRect.rect.left,
+            right: gridRect.right - tileRect.rect.right
+        })
+   }
+ 
     return(
-        <Draggable 
+        <DraggableCore 
             key={id} 
             index={index}
             defaultPosition={{x: 0, y: 0}}
             position={{x:0, y:0}}
-            onStart={onStart}
+            onMouseDown ={onMouseDown}
+            onStart = {onStart}
+            onStop = {onStop}
             onDrag = {onDrag}
-            onStop = {onDragEnd}
             nodeRef={nodeRef}
-            bounds = {tileBounds}
+            bounds = {bounds}
            >
-                <div id={id} className="gridTiles d-flex align-items-center justify-content-center col-5 col-sm-3 col-lg-2" ref={nodeRef} >
-                    <p>{content}</p>
+                <div id={id} 
+                    className="gridTiles d-flex align-items-center justify-content-center col-5 col-sm-3 col-lg-2" 
+                    content={content} 
+                    ref={nodeRef} >
+                        <p>{content}</p>
                 </div>          
-        </Draggable>
+        </DraggableCore>
     )
 }
 export default GridTiles
--- frontend/src/app/components/gameTypes/MatchActivity/MatchActivityApp.js
+++ frontend/src/app/components/gameTypes/MatchActivity/MatchActivityApp.js
@@ -1,22 +1,41 @@
 import React, {useState, useRef, useEffect} from 'react'
 import GridTiles from './GridTiles'
-
+/*
+To-dos
+Backend: 
+    1. Missing updating the backend with partial completion of assignment
+    2. Missing updating the backend with grade after completion
+Frontend:
+    1. Adding selecting logic for activity to work on mobile device
+        - Relevant code affecting this, is located in onStart 
+          and onDrag function and nowhere else. 
+          onStop function logic does not need to be changed
+            -The issue arises because on mobile, startEl and
+             finalEl will be selecting the element being dragged
+             when only the startEl should be selecting it.
+             The finalEl SHOULD be selecting the element underneath
+             the one being dragged
+    2. Add animation transition when elements are removed 
+    3. Missing answer validation (check if their sorting is correct)
+    4. Missing re-rendering logic, when user answers question and moves on to the next one.
+    5. Missing progress saved on local storage/memory (if user exits out of page)
+*/
 const activityData = {
     1: {
-        "Hi":"Okay", 
-        "Okay":"Hi", 
-        "Bye":"No", 
-        "No":"Bye",
-        "Cool":"But", 
-        "But":"Cool",
+        "Pair1":"Pair1-", 
+        "Pair2":"Pair2-", 
+        "Pair3":"Pair3-", 
+        "Pair1-":"Pair1",
+        "Pair2-":"Pair2", 
+        "Pair3-":"Pair3",
     },
 }
 const MatchActivityApp = () => {
     const currQuestion = 1
-    const [state, setstate] = useState(activityData)
-    const [position, setPosition] = useState({x:"0", y:"0"})
+    const [state, setState] = useState(activityData)
+    
     const nodeRef = useRef(null)
-    const size = useGridSize();
+    const gridSize = useGridSize();
 
     // Hook for resizing of Grid
     function useGridSize() {
@@ -39,25 +58,59 @@ const MatchActivityApp = () => {
         return gridSize;
     }
 
-    const onDragEnd = (e, data) => {
-        //if the elements are not considered a matching pair
-       console.log(e)
-        //if the elements are considered a matching pair
+    let startEl
+    let finalEl
+
+    const onStart =(e) =>{
+        //updates selected element
+        startEl = e.target.closest("div") 
+        //disable events for selected elements
+        startEl.style.pointerEvents = "none"
     }
+    const onDrag = (e) =>{
+        //updates the overlapping element
+        finalEl = e.target.closest("div")
+    }
+    
+    const onStop = () => {
+        const final = !finalEl ? null: finalEl.getAttribute("content")
+        const start = startEl.getAttribute("content")
+        const newMatchList = Object.assign({}, state[currQuestion])
+        
+        //restore events for selected element
+        startEl.style.pointerEvents = "all"
+
+        //when elements are not considered a matching pair
+        //it will simply return item to original position
+        if(state[currQuestion][start] !== final) return
+
+       //when elements are considered a matching pair
+       //it will remove the element pair
+        delete newMatchList[start]
+        delete newMatchList[final]
+        setState({
+            ...state,
+            [currQuestion]: newMatchList
+        })
+    }
+
+    
     return(
         <div className="matchActivityApp d-flex flex-column align-items-center">
-            <p>Match the following Tiles</p>
+            <p className="matchInstruction">Match the following</p>
             <div className = "gridLayout d-flex justify-content-center flex-wrap">
                 {Object.keys(state[currQuestion]).map((content, index)=>{
                     return <GridTiles 
-                                size ={size}
-                                onDragEnd = {onDragEnd}
+                                gridSize ={gridSize}
+                                onStop = {onStop}
+                                onDrag = {onDrag}
+                                onStart = {onStart}
                                 key={index} 
                                 id={index} 
                                 index ={index}
                                 content = {content}
-                                nodeRef = {nodeRef} 
-                                {...state}/>
+                                nodeRef = {nodeRef}
+                                />
                 })}
             </div>
         </div>
