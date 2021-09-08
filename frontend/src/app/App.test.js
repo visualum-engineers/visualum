83b87bcc6f2a35292d5a57ed174df5a7c75f4ea1
Arky Asmal <arkyasmal@gmail.com>
2021-09-07 22:19:55 -0400

    "added new default test to app"

--- frontend/src/app/App.test.js
+++ frontend/src/app/App.test.js
@@ -4,3 +4,9 @@ import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 import App from './App';
 
+describe("<App />", () => {
+  it("Renders <App /> component correctly", () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/visualum/i)).toBeInTheDocument();
+  });
+});
