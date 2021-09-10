255f0794cd8cde9dc0207200d5df56cb235ba2fe
Arky Asmal <arkyasmal@gmail.com>
2021-09-09 23:53:21 -0400

    "mutiple choice beta framework built"

--- frontend/src/app/components/gameTypes/MultipleChoice/MultipleChoiceApp.js
+++ frontend/src/app/components/gameTypes/MultipleChoice/MultipleChoiceApp.js
@@ -1,22 +1,29 @@
 import React, {useState} from 'react'
+import NavActivityBtn from '../NavActivityBtn/NavActivityBtn'
 const activityData = {
     "1": "What faces this world?"
 }
-const MultipleChoiceApp = () => {    
-    const [state, setState] = useState(activityData)
-    const [question, setQuestion] = useState(1)
-    const onClick = () => {
-        setQuestion(state + 1)
-    }
+const MultipleChoiceApp = ({last=false, onClick}) => {    
+    const [state] = useState(activityData)
+    const prevQuestion = Object.keys(activityData)[0] === "1" ? false : true
+    const lastQuestion = last
     return(
-        <div className = "multipleChoiceApp flex-column">
-            <div className = "MCQuestion">
-                <p>{state[question]}</p>  
+        <div className = "multipleChoiceApp d-flex justify-content-center">
+            <div className = "d-flex flex-column align-items-center col-9 col-md-6 col-xl-4 flex-column">
+                <div className = "MCQuestion">
+                    <p>{state[Object.keys(state)[0]]}</p>  
+                </div>
+                <div className = "MCInputContainer">
+                    
+                </div>
+                {/*loads appropriate btns depending if 
+                        1. There are prev questions
+                        2. This is the last questions */}
+                <div className = {`w-100 d-flex ${!prevQuestion ? "justify-content-end":"justify-content-between"}`}>
+                    {prevQuestion ? <NavActivityBtn onClick = {onClick} prev={prevQuestion}/>: null}
+                    <NavActivityBtn onClick = {onClick} last={lastQuestion}/>
+                </div>
             </div>
-            <div className = "MCInputContainer">
-                
-            </div>
-            <button onClick ={onClick}>Submit</button>
         </div>
     )
 } 
