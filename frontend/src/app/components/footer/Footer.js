e440ff28589fa433fb36676c383bea54e2b15bf5
Arky Asmal <arkyasmal@gmail.com>
2021-09-22 17:13:53 -0400

    "change names capitalization"

---
+++ frontend/src/app/components/footer/Footer.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+//Automates changing year
+let d = new Date()
+let time = d.getFullYear()
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer className="container">
+                <div id="footer" className="d-flex flex-wrap py-2">
+                    <div className="col-sm-4 col-12 text-center py-3">
+                        <h5>Visualum</h5>
+                        <div className="d-flex flex-column align-items-center">
+                            <a className="foot-link" href="#footer">For Teachers</a>
+                            <a className="foot-link" href="#footer">For Students</a>
+                            <a className="foot-link" href="#footer">For Enterprise</a>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4 col-12 text-center py-3">
+                        <h5>Social</h5>
+                        <div className="d-flex flex-column align-items-center">
+                            <a className="foot-link" href="#footer">LinkedIn</a>
+                            <a className="foot-link" href="#footer">Instagram</a>
+                            <a className="foot-link" href="#footer">Facebook</a>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4 col-12 text-center py-3">
+                        <h5>About</h5>
+                        <div className="d-flex flex-column align-items-center">
+                            <a className="foot-link" href="#footer">Terms</a>
+                            <a className="foot-link" href="#footer">Contact Us</a>
+                            <a className="foot-link" href="#footer">Meet the Team</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="copyright" className="row">
+                    <div className="text-center">
+                        <p><i className="fa fa-copyright"></i> {time} Visualum All rights reserved </p>
+                    </div>
+                </div>
+            </footer>
+
+        )
+    }
+}
