8f33041da29f1a120365525767ef2d1acef39952
Derek Widmer <dwidmer99@gmail.com>
2021-09-29 14:34:03 -0400

    Add functionality to 'Go back' button in dashboard nav

--- frontend/src/app/components/dashboard/DashboardNav.js
+++ frontend/src/app/components/dashboard/DashboardNav.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 
 export default function DashboardNav(props) {
+    let history = useHistory();
+
+    function handleClick() {
+        history.push("/");
+    }
     return (
         <nav className="navbar navbar-dark bg-light dashboard-nav">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#navbar">visualum</a>
                 <div className="back-button-container">
-                    <button className="btn back-button">
+                    <button className="btn back-button" onClick={handleClick}>
                         Go Back
                     </button>
                 </div>
