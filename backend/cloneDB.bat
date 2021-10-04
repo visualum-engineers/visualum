3572fbadfbcffc002d49ad67888865c63409f0d0
Arky <arkyasmal@gmail.com>
2021-10-04 03:23:42 -0400

    "completed automated script to run start backend server with 1 command, for windows"

--- backend/cloneDB.bat
+++
@@ -1,7 +0,0 @@
-:: clones database
-for /f "delims=" %%i in ('doppler secrets get MONGO_DB_URI --plain') do set MONGO_DB_URI=%%i
-echo %MONGO_DB_URI%
-mongodump --uri "%MONGO_DB_URI%" --out ./cloneDB
-mongorestore --port 27017 ./cloneDB
-:: makes env variable available to authenticated members
-doppler run -- nodemon app.js
