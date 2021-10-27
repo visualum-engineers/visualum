9a76ab79c6be9e6bd47c2421aac1011c503a4a95
Arky <arkyasmal@gmail.com>
2021-10-27 19:01:44 -0400

    "added lazy loading for webpages"

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import NavWrapper from './components/primaryNavbar/NavWrapper';
 
 import DashboardHome from './components/dashboard/DashboardHome'
@@ -6,88 +9,93 @@ import DashboardActivities from './components/dashboard/DashboardActivities';
 import DashboardClasses from './components/dashboard/DashboardClasses';
 import DashboardClass from './components/dashboard/DashboardClass';
 
-import HomeContent from './components/homePage/Home';
-import Settings from './components/settingsPage/Settings';
-import SignUpForm from './components/forms/signUpForm';
-import LoginForm from './components/forms/logInForm';
-import CreateGame from './components/createGame/CreateGame';
-import Activity from './components/activityTypes/Activity'
-import LabelPictures from "./components/activityTypes/LabelPictures/LabelPicturesApp"
 import sampleActivityData from './helpers/sampleActivityData'
 import DashboardAssignments from './components/dashboard/DashboardAssignments';
 import TestBackend from "./components/testBackend/test"
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+//lazy loaded components for performance
+const Settings = React.lazy(()=> import('./components/settingsPage/Settings'))
+const HomeContent = React.lazy(()=> import('./components/homePage/Home'))
+const SignUpForm = React.lazy(()=> import('./components/forms/signUpForm'))
+const LoginForm = React.lazy(()=> import('./components/forms/logInForm'))
+const CreateGame = React.lazy(()=> import('./components/createGame/CreateGame'))
+const Activity = React.lazy(()=> import('./components/activityTypes/Activity'))
+const LabelPictures = React.lazy(()=> import("./components/activityTypes/LabelPictures/LabelPicturesApp"))
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
+        <Suspense fallback={
+          //will replace with a spinner icon like Netflix has later.
+          <p>Loading...</p>
+        }>
+          <Switch>
+            <Route
+              exact path="/">
+              <NavWrapper>
+                <HomeContent />
+              </NavWrapper>
+            </Route>
 
-          <Route
-            exact path="/">
-            <NavWrapper>
-              <HomeContent />
-            </NavWrapper>
-          </Route>
+            <Route exact path="/signup">
+              <NavWrapper>
+                <SignUpForm />
+              </NavWrapper>
+            </Route>
 
-          <Route exact path="/signup">
-            <NavWrapper>
-              <SignUpForm />
-            </NavWrapper>
-          </Route>
+            <Route exact path="/login">
+              <NavWrapper>
+                <LoginForm />
+              </NavWrapper>
+            </Route>
 
-          <Route exact path="/login">
-            <NavWrapper>
-              <LoginForm />
-            </NavWrapper>
-          </Route>
+            <Route exact path="/settings">
+              <NavWrapper>
+                <Settings />
+              </NavWrapper>
+            </Route>
 
-          <Route exact path="/settings">
-            <NavWrapper>
-              <Settings />
-            </NavWrapper>
-          </Route>
+            <Route exact path="/create-game">
+              <NavWrapper>
+                <CreateGame />
+              </NavWrapper>
+            </Route>
 
-          <Route exact path="/create-game">
-            <NavWrapper>
-              <CreateGame />
-            </NavWrapper>
-          </Route>
+            <Route exact path="/activity">
+                <Activity />
+            </Route>
 
-          <Route exact path="/activity">
-              <Activity />
-          </Route>
+            <Route exact path="/label-pictures">
+              <NavWrapper>
+                <LabelPictures />
+              </NavWrapper>
+            </Route>
 
-          <Route exact path="/label-pictures">
-            <NavWrapper>
-              <LabelPictures />
-            </NavWrapper>
-          </Route>
+            <Route exact path="/dashboard/home">
+              <DashboardHome />
+            </Route>
+            <Route exact path="/dashboard/activities">
+              <DashboardActivities data={sampleActivityData} />
+            </Route>
+            <Route exact path="/dashboard/assignments">
+              <DashboardAssignments />
+            </Route>
+            <Route exact path="/dashboard/classes/:class_id">
+              <DashboardClass />
+            </Route>
+            <Route exact path="/dashboard/classes">
+              <DashboardClasses />
+            </Route>
+            <Route path="/dashboard">
+              <DashboardHome />
+            </Route>
 
-          <Route exact path="/dashboard/home">
-            <DashboardHome />
-          </Route>
-          <Route exact path="/dashboard/activities">
-            <DashboardActivities data={sampleActivityData} />
-          </Route>
-          <Route exact path="/dashboard/assignments">
-            <DashboardAssignments />
-          </Route>
-          <Route exact path="/dashboard/classes/:class_id">
-            <DashboardClass />
-          </Route>
-          <Route exact path="/dashboard/classes">
-            <DashboardClasses />
-          </Route>
-          <Route path="/dashboard">
-            <DashboardHome />
-          </Route>
-
-          <Route exact path = "/testBackend">
-            <TestBackend/>
-          </Route>
-        </Switch>
+            <Route exact path = "/testBackend">
+              <TestBackend/>
+            </Route>
+          </Switch>
+        </Suspense>
       </div >
     </Router >
   );
