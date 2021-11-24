b47765714dc054f115fc9cc9738bca4db2eba20a
Derek Widmer <dwidmer99@gmail.com>
2021-11-23 20:28:03 -0500

    Fixed conflicts in mixins

--- frontend/src/redux/features/dashboardStatus/dashboardSlice.js
+++ frontend/src/redux/features/dashboardStatus/dashboardSlice.js
@@ -3,16 +3,21 @@ import { createSlice } from '@reduxjs/toolkit'
 export const dashboardSlice = createSlice({
     name: 'dashboard',
     initialState: {
-        selected: "home"
+        selected: "home",
+        collapsed: false,
+        sidebarClass: "collapsable-sidebar"
     },
     reducers: {
         updated: (state, action) => {
             state.selected = action.payload
+        },
+        toggledCollapsed: (state, action) => {
+            state.collapsed = !state.collapsed
         }
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { updated } = dashboardSlice.actions
+export const { updated, toggledCollapsed } = dashboardSlice.actions
 
 export default dashboardSlice.reducer
