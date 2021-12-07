c705d047228bf437f64eb14d9cbf968d004cfb26
Derek Widmer <dwidmer99@gmail.com>
2021-12-07 14:42:56 -0500

    Shrunk styles

--- frontend/src/app/components/dashboard/OverviewCard.js
+++ frontend/src/app/components/dashboard/OverviewCard.js
@@ -12,7 +12,7 @@ export default function OverviewCard(props) {
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{props.subtitle}</h6>
-                <div className="overview-card-feature">{props.children}</div>
+                {/* <div className="overview-card-feature">{props.children}</div> */}
             </div>
         </div>
     )
