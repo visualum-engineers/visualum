3e11f050715b093e78ee5a468c0189bcca4117a4
Derek Widmer <dwidmer99@gmail.com>
2021-12-10 01:44:14 -0500

    Updated features section of homepage

--- frontend/src/app/components/homePage/IntroHome.js
+++ frontend/src/app/components/homePage/IntroHome.js
@@ -2,30 +2,6 @@ const background = "images/homePage/background.jpeg"
 
 const IntroHome = () => {
     return (
-        // <div className="intro-homePage d-flex flex-column align-items-center">
-        //     <img src={mountainHomebg} alt="Flag on Montain" className="homePageImage" />
-        //     <div className="d-flex justify-content-center align-items-center h-100">
-        //         <div className="introContainer d-flex flex-column align-items-center">
-        //             <h2 className="homePageText">A world class education for anyone, anywhere.</h2>
-        //             <p className="homePageText">Join us for an interactive</p>
-        //             <p className="homePageText">learning experience</p> <br />
-        //             <div className="d-flex justify-content-center">
-        //                 <button
-        //                     id="signup-btn"
-        //                     className="btn btn-outline-light btn-lg mx-1">
-        //                     <span>Sign Up
-        //                         <i className="fa fa-caret-right"></i>
-        //                     </span>
-        //                 </button>
-        //             </div>
-        //         </div>
-        //     </div>
-
-        //     <a href="#home-page-market-intro" className="home-explore-link d-flex flex-column align-items-center">
-        //         <p>Explore Our Library of Activities</p>
-        //         <i className="fas fa-angle-down"></i>
-        //     </a>
-        // </div>
         <div className="intro">
             <div className="intro-homepage" style={{ backgroundImage: background }}>
                 <div className="intro-blurb">
--- frontend/src/app/components/homePage/MarketingHomeBody.js
+++ frontend/src/app/components/homePage/MarketingHomeBody.js
@@ -1,15 +1,15 @@
-const halfMoonBg = "./images/homePage/halfMoonBg.jpg"
+// const halfMoonBg = "./images/homePage/halfMoonBg.jpg"
 const MarketingHomeBody = () =>{
     return(
         <div 
             id="home-page-market-body" 
             className=" d-flex flex-column align-items-center"
         >
-           <img 
+           {/* <img 
                 src={halfMoonBg} 
                 alt={"Half moon night sky"} 
                 className="home-page-market-body-bg" 
-            />
+            /> */}
         </div>
     )
 }
