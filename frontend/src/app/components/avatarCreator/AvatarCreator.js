faba4c886700b0b621aa7b39e3fca42a2fc0713b
Derek Widmer <dwidmer99@gmail.com>
2021-12-15 19:34:05 -0500

    Implement types and state of avatar

--- frontend/src/app/components/avatarCreator/AvatarCreator.js
+++ frontend/src/app/components/avatarCreator/AvatarCreator.js
@@ -1,48 +1,89 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Dashboard from '../dashboard/Dashboard'
 import Avatar from 'avataaars'
-import hairStyles from './types'
+import {
+	hairstyle,
+	accessory,
+	clothes,
+	eyebrow,
+	eyes,
+	facialHair,
+	mouth,
+	skinColor,
+	hairColor,
+	facialHairColor,
+	clotheColor
+} from './types'
 
-
-
-const hairStylesLength = hairStyles.length;
-
+const hairstyleLength = hairstyle.length;
+const accessoryLength = accessory.length;
+const clothesLength = clothes.length;
+const eyebrowLength = eyebrow.length;
+const eyesLength = eyes.length;
+const facialHairLength = facialHair.length;
+const mouthLength = mouth.length;
+const skinColorLength = skinColor.length;
+const hairColorLength = hairColor.length;
+const facialHairColorLength = facialHairColor.length;
+const clotheColorLength = clotheColor.length;
 
 export default function AvatarCreator() {
 
 	const [options, setOptions] = useState(
 		{
-			topType: 0,
-			accessoriesType: 0,
-			hairColor: 0,
-			facialHairType: 0,
-			clotheType: 0,
-			clotheColor: 0,
-			eyeType: 0,
-			eyebrowType: 0,
-			mouthType: 0,
-			skinColor: 0
+			topType: Math.floor(Math.random() * hairstyleLength),
+			accessoriesType: Math.floor(Math.random() * accessoryLength),
+			hairColor: Math.floor(Math.random() * hairColorLength),
+			facialHairType: Math.floor(Math.random() * facialHairLength),
+			clotheType: Math.floor(Math.random() * clothesLength),
+			clotheColor: Math.floor(Math.random() * clotheColorLength),
+			eyeType: Math.floor(Math.random() * eyesLength),
+			eyebrowType: Math.floor(Math.random() * eyebrowLength),
+			mouthType: Math.floor(Math.random() * mouthLength),
+			skinColor: Math.floor(Math.random() * skinColorLength),
+			facialHairColor: Math.floor(Math.random() * facialHairColorLength),
 		}
 	)
 
-	// useEffect(() => {
-	// 	console.log(options)
-	// }, [options])
-
-	const incrementValue = (key) => {
+	const changeValue = (key, method = "inc") => {
 		setOptions(curr => {
 			//set max length to detect overlap
 			let max_length = 0;
-			if (key == "topType") {
-				max_length = hairStylesLength
-			} else if (key = "accessoriesType") {
-				max_length = 10;
+			if (key === "topType") {
+				max_length = hairstyleLength
+			} else if (key === "accessoriesType") {
+				max_length = accessoryLength;
+			} else if (key === "hairColor") {
+				max_length = hairColorLength
+			} else if (key === "facialHairType") {
+				max_length = facialHairLength
+			} else if (key === "clotheType") {
+				max_length = clothesLength
+			} else if (key === "clotheColor") {
+				max_length = clotheColorLength
+			} else if (key === "eyeType") {
+				max_length = eyesLength
+			} else if (key === "eyebrowType") {
+				max_length = eyebrowLength
+			} else if (key === "mouthType") {
+				max_length = mouthLength
+			} else if (key === "skinColor") {
+				max_length = skinColorLength
+			} else if (key === "facialHairColor") {
+				max_length = facialHairColorLength
 			}
-			//increment the value
-			let newVal = curr[key] + 1
+			//change the value based on method
+			let newVal = 0;
+			if (method === "inc") {
+				newVal = curr[key] + 1
+			} else if (method === "dec") {
+				newVal = curr[key] - 1
+			}
 			//if out of bounds, set to 0
 			if (curr[key] >= max_length) {
 				newVal = 0
+			} else if (curr[key] < 0) {
+				newVal = max_length - 1
 			}
 			curr[key] = newVal;
 			return { ...curr };
@@ -54,20 +95,18 @@ export default function AvatarCreator() {
 			<Avatar
 				style={{ width: '150px', height: '150px' }}
 				avatarStyle='Circle'
-				topType={hairStyles[options.topType]}
-				accessoriesType='Prescription02'
-				hairColor='BrownDark'
-				facialHairType='Blank'
-				clotheType='Hoodie'
-				clotheColor='PastelBlue'
-				eyeType='Happy'
-				eyebrowType='Default'
-				mouthType='Smile'
-				skinColor='Light'
+				topType={hairstyle[options.topType]}
+				accessoriesType={accessory[options.accessoriesType]}
+				hairColor={hairColor[options.hairColor]}
+				facialHairType={facialHair[options.facialHairType]}
+				facialHairColor={facialHairColor[options.facialHairColor]}
+				clotheType={clothes[options.clotheType]}
+				clotheColor={clotheColor[options.clotheColor]}
+				eyeType={eyes[options.eyeType]}
+				eyebrowType={eyebrow[options.eyebrowType]}
+				mouthType={mouth[options.mouthType]}
+				skinColor={skinColor[options.skinColor]}
 			/>
-			<button onClick={() => incrementValue("topType")}>Increment Top</button>
-
-
 		</Dashboard >
 	)
 }
--- frontend/src/app/components/avatarCreator/types.js
+++ frontend/src/app/components/avatarCreator/types.js
@@ -1,19 +1,164 @@
-const hairStyles = ["noHair", "Eyepatch", "Hat", "Hijab", "WinterHat1", "WinterHat2", "WinterHat3", "WinterHat4",
-	"LongHairBigHair", "LongHairBob", "LongHairBun", "LongHairCurly", "LongHairCurvy", "LongHairDreads", "LongHairFrida",
-	"LongHairFro", "LongHairFroBand", "LongHairNotTooLong", "LongHairShavedSides", "LongHairMiaWallace", "LongHairStraight",
-	"LongHairStraightStrand", "ShortHairDreads01", "ShortHairDreads02", "ShortHairFrizzle", "ShortHairShaggyMullet",
-	"ShortHairShortCurly", "ShortHairShortFlat", "ShortHairShortRound", "ShortHairShortWaved", "ShortHairTheCaesar",
-	"ShortHairTheCaesarSidePart"]
+const hairstyle = [
+	"noHair",
+	"Eyepatch",
+	"Hat",
+	"Hijab",
+	"WinterHat1",
+	"WinterHat2",
+	"WinterHat3",
+	"WinterHat4",
+	"LongHairBigHair",
+	"LongHairBob",
+	"LongHairBun",
+	"LongHairCurly",
+	"LongHairCurvy",
+	"LongHairDreads",
+	"LongHairFrida",
+	"LongHairFro",
+	"LongHairFroBand",
+	"LongHairNotTooLong",
+	"LongHairShavedSides",
+	"LongHairMiaWallace",
+	"LongHairStraight",
+	"LongHairStraightStrand",
+	"ShortHairDreads01",
+	"ShortHairDreads02",
+	"ShortHairFrizzle",
+	"ShortHairShaggyMullet",
+	"ShortHairShortCurly",
+	"ShortHairShortFlat",
+	"ShortHairShortRound",
+	"ShortHairShortWaved",
+	"ShortHairTheCaesar",
+	"ShortHairTheCaesarSidePart",
+]
 
-const accessories = [
+const hairColor = [
+	"Auburn",
+	"Black",
+	"Blonde",
+	"BlondeGolden",
+	"Brown",
+	"BrownDark",
+	"PastelPink",
+	"Blue",
+	"Platinum",
+	"Red",
+	"SilverGray",
+]
+
+const accessory = [
 	"Kurt",
 	"Prescription01",
 	"Prescription02",
 	"Round",
 	"Sunglasses",
-	"Wayfarers"
+	"Wayfarers",
 ]
 
-const
+const facialHair = [
+	"Blank",
+	"BeardLight",
+	"BeardMedium",
+	"BeardMagestic",
+	"MoustacheFancy",
+	"MoustacheMagnum",
+]
 
-export { hairStyles, accessories };
+const facialHairColor = [
+	"Auburn",
+	"Black",
+	"Blonde",
+	"BlondeGolden",
+	"Brown",
+	"BrownDark",
+	"PastelPink",
+	"Blue",
+	"Platinum",
+	"Red",
+]
+
+const clothes = [
+	"BlazerShirt",
+	"BlazerSweater",
+	"CollarSweater",
+	"GraphicShirt",
+	"Hoodie",
+	"Overall",
+	"ShirtCrewNeck",
+	"ShirtScoopNeck",
+	"ShirtVNeck",
+]
+
+const clotheColor = [
+	"Black",
+	"Blue01",
+	"Blue02",
+	"Blue03",
+	"Gray01",
+	"Gray02",
+	"Heather",
+	"PastelBlue",
+	"PastelGreen",
+	"PastelOrange",
+	"PastelRed",
+	"PastelYellow",
+	"Pink",
+	"Red",
+	"White",
+]
+const eyes = [
+	"Close",
+	"Cry",
+	"Default",
+	"Dizzy",
+	"EyeRoll",
+	"Happy",
+	"Hearts",
+	"Side",
+	"Squint",
+	"Surprise",
+	"Wink",
+	"WinkWacky",
+]
+
+const eyebrow = [
+	"Angry",
+	"AngryNatural",
+	"Default",
+	"DefaultNatural",
+	"FlatNatural",
+	"RaisedExcited",
+	"RaisedExcitedNatural",
+	"SadConcerned",
+	"SadConcernedNatural",
+	"UnibrowNatural",
+	"UpDown",
+	"UpDownNatural",
+]
+
+const mouth = [
+	"Concerned",
+	"Default",
+	"Disbelief",
+	"Eating",
+	"Grimace",
+	"Sad",
+	"ScreamOpen",
+	"Serious",
+	"Smile",
+	"Tongue",
+	"Twinkle",
+]
+
+const skinColor = [
+	"Pale",
+	"Yellow",
+	"Light",
+	"Tanned",
+	"Brown",
+	"DarkBrown",
+	"Black",
+]
+
+export { hairstyle, accessory, facialHair, clothes, eyes, eyebrow, mouth, skinColor, hairColor, facialHairColor, clotheColor };
