62d8b0eaa9ce5c9a803aceabba8ef7c1c153f6dd
Arky Asmal <arkyasmal@gmail.com>
2022-01-18 12:28:00 -0500

    "added proper workflow scripts to provide secrets"

--- frontend/src/app/RealmApp.js
+++ frontend/src/app/RealmApp.js
@@ -2,7 +2,8 @@ import React from "react";
 import * as Realm from "realm-web";
 //import { useDispatch } from "react-redux";
 //only needed upon app initialization
-const appId = process.env.REACT_APP_REALM_APP_ID
+const appId = process.env["REACT_APP_REALM_APP_ID"]
+console.log(process.env)
 const RealmAppContext = React.createContext();
 
 export const useRealmApp = () => {
