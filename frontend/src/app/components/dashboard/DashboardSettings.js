865ddd55bbd51d9ea86a79c0343b9af2407e0ba1
Derek Widmer <dwidmer99@gmail.com>
2022-01-23 20:08:57 -0500

    Added basic settings modals

--- frontend/src/app/components/dashboard/DashboardSettings.js
+++ frontend/src/app/components/dashboard/DashboardSettings.js
@@ -2,20 +2,46 @@ import React, { useState } from 'react'
 import AvatarCreator from '../avatarCreator/AvatarCreator';
 import SettingsCard from './SettingsCard';
 import CardContainer from './CardContainer'
+import SettingsEmailModal from './SettingsEmailModal';
+import SettingsAccountDetailsModal from './SettingsAccountDetailsModal';
 
 export default function DashboardSettings() {
 
+	const [currentModal, setCurrentModal] = useState(null)
 	const [modalOpen, setModalOpen] = useState(false);
 
 	const openAvatarModal = () => {
-		setModalOpen(true)
+		setCurrentModal(
+			<AvatarCreator onCancel={() => setModalOpen(false)}
+				onSubmit={() => console.log('Fill this in!')}
+			/>
+		);
+		setModalOpen(true);
 	};
 
+	const openEmailModal = () => {
+		setCurrentModal(
+			<SettingsEmailModal
+				handleClose={() => setModalOpen(false)}
+			/>
+		);
+		setModalOpen(true);
+	}
+
+	const openAccountDetailsModal = () => {
+		setCurrentModal(
+			<SettingsAccountDetailsModal
+				handleClose={() => setModalOpen(false)}
+			/>
+		);
+		setModalOpen(true);
+	}
+
 	const settings = [
-		{ title: 'Reset Password', subtitle: 'Forgot Your Password? Reset It Here.' },
+		{ title: 'Reset Password', subtitle: 'Forgot Your Password? Click for a reset code.' },
 		{ title: 'Your Avatar', subtitle: 'Switch Up Your Look!', handleClick: openAvatarModal },
-		{ title: 'Change Email', subtitle: 'Change which email is associated with your account.' },
-		{ title: 'Account Details', subtitle: 'Change the name and personal details associated with your account.' }
+		{ title: 'Change Email', subtitle: 'Change which email is associated with your account.', handleClick: openEmailModal },
+		{ title: 'Account Details', subtitle: 'Change the name and personal details associated with your account.', handleClick: openAccountDetailsModal }
 	]
 
 	const cards = settings.map(item =>
@@ -26,12 +52,7 @@ export default function DashboardSettings() {
 
 	return (
 		<div>
-			{modalOpen ? <AvatarCreator
-				onCancel={() => setModalOpen(false)}
-				onSubmit={() => console.log('Fill this in!')}
-			/> :
-				<></>
-			}
+			{modalOpen ? currentModal : <></>}
 			<CardContainer>
 				{cards}
 			</CardContainer>
