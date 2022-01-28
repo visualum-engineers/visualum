fb009a9737715278bd26194877a2f26759232acb
Arky Asmal <arkyasmal@gmail.com>
2022-01-27 21:34:00 -0500

    "complete mapping of activity setting btn and options. Client side validation for user feedback is left"

--- frontend/src/app/components/activityTypes/ActivitySettings.js
+++ frontend/src/app/components/activityTypes/ActivitySettings.js
@@ -1,7 +1,7 @@
 import PopUpBg from "../utilities/popUp/PopUpBackground"
 import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { enableSettings, disableSettings } from "../../../redux/features/activityTypes/activitiesSettings"
+import { enableSettings, disableSettings, changeTimeDuration } from "../../../redux/features/activityTypes/activitiesSettings"
 import { unstable_batchedUpdates } from "react-dom"
 import SwitchToggler from "../utilities/switchToggler/SwitchToggler"
 
@@ -12,7 +12,9 @@ const ActivitySettings = ({
     const disableDnD = useSelector((state) => state.activities.settings.dndEnabled) 
     const disableTimeReminders = useSelector((state) => state.activities.settings.timeRemindersEnabled)
     const disableAutoPopUps = useSelector((state) => state.activities.settings.autoPopUpsEnabled)
- 
+    const timeIntervalDuration = useSelector((state) => state.activities.settings.timeIntervalDuration)
+    const [timeDuration, setTimeDuration] = useState(timeIntervalDuration)
+    const [timeDurationErr, setTimeDurationErr] = useState(null)
     const [settingsActive, setSettingsActive] = useState({
         dndEnabled : disableDnD,
         timeRemindersEnabled : disableAutoPopUps,
@@ -27,10 +29,12 @@ const ActivitySettings = ({
                 timeRemindersEnabled : disableAutoPopUps,
                 autoPopUpsEnabled : disableTimeReminders, 
             })
+            setTimeDuration(timeIntervalDuration)
         }
         return () => {isMounted = false}
-    }, [disableDnD, disableTimeReminders, disableAutoPopUps])
+    }, [disableDnD, disableTimeReminders, disableAutoPopUps, timeIntervalDuration])
     const onSaveBtnClick = () =>{
+            if(timeDurationErr) return
             let disabledSettings = []
             let enabledSettings = []
             Object.keys(settingsActive).map((value)=>{
@@ -38,6 +42,13 @@ const ActivitySettings = ({
                 if(!settingsActive[value]) disabledSettings.push(value)
                 return null
             })
+            const checkTimeInterval = timeIntervalDuration !== timeDuration 
+                                        && settingsActive.timeRemindersEnabled 
+                                        && (timeDuration !== 0 || timeDuration!=="0") 
+            //save time interval if this is enabled
+            if(checkTimeInterval) dispatch(changeTimeDuration(timeDuration))
+
+            //update enabled or disabled settings
             unstable_batchedUpdates(()=>{
                 dispatch(enableSettings(enabledSettings))
                 dispatch(disableSettings(disabledSettings))
@@ -75,8 +86,9 @@ const ActivitySettings = ({
                         
                     </div>
                     <div className="activity-settings-pop-up-body">
-                            <div className="settings-body-row">
-                                <div className="settings-option-text">
+                            <div className={`settings-body-row`
+                                            + `${settingsActive.timeRemindersEnabled ? " activity-settings-time": ""}`}>
+                                <div className="settings-option-text justify-content-center">
                                     <label htmlFor="activity-settings-disable-time">
                                         Disable Time Reminders
                                     </label>
@@ -90,8 +102,46 @@ const ActivitySettings = ({
                                         switchOffAriaLabel={"enable-time-reminders"}
                                         switchOn = {!settingsActive.timeRemindersEnabled}
                                     />
+                                   
                                 </div>
                             </div>
+                            {settingsActive.timeRemindersEnabled &&
+                                <div className="activity-settings-time-options">
+                                    <div className="d-flex justify-content-between">
+                                        <div className="settings-option-text w-50">
+                                            <label className=" activity-settings-time-interval">
+                                                How often do you want time reminders?
+                                            </label>
+                                        </div>
+                                        <div className="d-flex align-items-center">
+                                            <input
+                                                type={"number"}
+                                                className="activity-settings-time-interval-input"
+                                                value={timeDuration/60/1000 === 0 ? "" : timeDuration/60/1000}
+                                                onChange={(e)=>{
+                                                    const target = e.target.closest("input")                                                
+                                                    const value = target.value
+                                                    if(value.length > 2) return
+                                                    setTimeDuration(value * 60 * 1000)
+                                                    setTimeDurationErr(null)
+                                                    //for validation
+                                                    if(value.length === 0 || value==="0") setTimeDurationErr("You must set a time, or disable time reminders") 
+                                                }}
+                                                onKeyDown={(e) =>{
+                                                    if(/[e+-]/gi.test(e.key) && e.key.length<=1) e.preventDefault()
+                                                }}
+                                            />
+                                            <span>mins</span>
+                                        </div>
+                                    </div>
+                                    
+                                    {timeDurationErr && <div 
+                                        className="alert alert-danger" 
+                                        style={{textAlign: "center", marginTop: "0.5rem", marginBottom: 0}}
+                                    >{timeDurationErr}
+                                    </div>}
+                                </div>
+                            }
                             
                             <div className="settings-body-row">
                                 <div className="settings-option-text">
@@ -114,6 +164,9 @@ const ActivitySettings = ({
                                         switchOn = {!settingsActive.autoPopUpsEnabled}
                                     />
                                 </div>
+                            </div>
+                            <div>
+                            
                             </div>
                             <div className="settings-body-row">
                                 <div className="settings-option-text">
