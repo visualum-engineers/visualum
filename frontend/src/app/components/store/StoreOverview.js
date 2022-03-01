afe924c774dc9e3d11bbf7f959799ca00376ef28
Derek Widmer <dwidmer99@gmail.com>
2022-02-28 23:41:50 -0500

    Add Show More button to item carousel in store

--- frontend/src/app/components/store/StoreOverview.js
+++ frontend/src/app/components/store/StoreOverview.js
@@ -8,6 +8,14 @@ import sampleData from './sampleData'
 
 export default function Store() {
 
+	const showMore = (id) => {
+		return (
+			<Link to={`${id}`} className='store-item store-carousel-item show-more-item d-flex justify-content-center align-items-center'>
+				<span className='fw-bold'>Show More</span>
+			</Link >
+		)
+	}
+
 	const sections = sampleData.map(store => {
 		return (
 			<div className='col-12 mb-5'>
@@ -17,11 +25,8 @@ export default function Store() {
 						<div className='col-lg-4 col-12 d-flex justify-content-lg-end justify-content-between align-items-center'>
 							<span className='h6 mb-0'>
 								<FontAwesomeIcon icon={faDollarSign} className="icon me-2" />
-								209 points remaining
+								209 pts
 							</span>
-							<Link to={`${store.class_id}`} className='btn btn-sm btn-primary ms-3'>
-								Show More
-							</Link>
 						</div>
 					</div>
 				</div>
@@ -34,6 +39,7 @@ export default function Store() {
 						</div>
 						<div className='store-item-container'>
 							{store.items.map(item => <StoreCarouselItem title={item.name} price={item.price} key={item.id} />)}
+							{showMore(store.class_id)}
 						</div>
 						<div className='carousel-button-container'>
 							<div className='carousel-button'>
@@ -50,66 +56,6 @@ export default function Store() {
 		<>
 			<h1 className='fw-bold mb-5'>All Stores</h1>
 			<div className='row'>
-				{/* <div className='col-12'>
-					<div className='d-flex align-items-center mb-2'>
-						<h2>Mrs. Teacher's Class Store</h2>
-						<span className='h6 mb-0 ms-auto'>209 points remaining</span>
-						<button className='btn btn-sm btn-primary ms-3'>
-							Show More
-						</button>
-					</div>
-					<div className='store-section'>
-						<div className='store-item-carousel'>
-							<div className='carousel-button-container'>
-								<div className='carousel-button'>
-									<FontAwesomeIcon icon={faAngleLeft} />
-								</div>
-							</div>
-							<div className='store-item-container'>
-								<StoreCarouselItem title="Homework Pass" price={50} />
-								<StoreCarouselItem title="Homework Pass" price={100} />
-								<StoreCarouselItem title="Homework Pass" price={1000} />
-								<StoreCarouselItem title="Homework Pass" price={1000} />
-								<StoreCarouselItem title="Homework Pass" price={1000} />
-								<StoreCarouselItem title="Homework Pass" price={1000} />
-								<StoreCarouselItem title="Homework Pass" price={1000} />
-								<StoreCarouselItem title="Homework Pass (1 Week)" price={50} />
-							</div>
-							<div className='carousel-button-container'>
-								<div className='carousel-button'>
-									<FontAwesomeIcon icon={faAngleRight} />
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className='d-flex align-items-center mt-5 mb-2'>
-						<h2>Teacher 1's Class </h2>
-						<span className='h6 mb-0 ms-auto'>209 points remaining</span>
-						<button className='btn btn-sm btn-primary ms-3'>
-							Show More
-						</button>
-					</div>
-				</div>
-				<div className='store-section mt-3'>
-					<div className='store-item-carousel'>
-						<div className='carousel-button-container'>
-							<div className='carousel-button'>
-								<FontAwesomeIcon icon={faAngleLeft} />
-							</div>
-						</div>
-						<div className='store-item-container'>
-							<StoreCarouselItem title="Homework Pass" price={50} />
-							<StoreCarouselItem title="Homework Pass" price={100} />
-							<StoreCarouselItem title="Homework Pass" price={1000} />
-							<StoreCarouselItem title="Homework Pass (1 Week)" price={50} />
-						</div>
-						<div className='carousel-button-container'>
-							<div className='carousel-button'>
-								<FontAwesomeIcon icon={faAngleRight} />
-							</div>
-						</div>
-					</div>
-				</div> */}
 				{sections}
 			</div>
 		</>
