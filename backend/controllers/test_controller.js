883629308114a6642b4485fed229b2f5e63bee1a
Arky Asmal <arkyasmal@gmail.com>
2022-03-20 19:35:14 -0400

    "remove backend directory. This repo will be for client side code only"

--- backend/controllers/test_controller.js
+++
@@ -1,5 +0,0 @@
-const message = (req, res) => {
-    res.json({message: "Post request has been sent"})
-};
-
-module.exports = {message};
--- backend/routes/test_router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const test_controller = require('../controllers/test_controller')
-
-router.get("/", test_controller.message)
-
-module.exports = router
