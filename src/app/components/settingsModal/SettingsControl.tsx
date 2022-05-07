a6f9177816c146cfedc874f64ca75abf80539fe9
aasmal97 <74555081+aasmal97@users.noreply.github.com>
2022-05-07 01:26:32 -0400

    "completed settings modal and settings page types"

--- src/app/components/settingsModal/SettingsControl.tsx
+++ src/app/components/settingsModal/SettingsControl.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
-export default function SettingsControl({ title, value, subtitle, linkText, link = null, action = null }) {
-	return (
-		<div className='settings-control'>
-			<h3 className='title'>{title}: <span className='value'>{value}</span></h3>
-			<span className='link' onClick={action}>{linkText}</span>
-			{subtitle ? <p className='subtitle'>{subtitle}</p> : null}
-		</div>
-	)
+export default function SettingsControl({
+  title,
+  value,
+  subtitle,
+  linkText,
+  link = null,
+  action = null,
+}: any) {
+  return (
+    <div className="settings-control">
+      <h3 className="title">
+        {title}: <span className="value">{value}</span>
+      </h3>
+      <span className="link" onClick={action}>
+        {linkText}
+      </span>
+      {subtitle ? <p className="subtitle">{subtitle}</p> : null}
+    </div>
+  );
 }
--- src/app/components/settingsModal/SettingsModal.tsx
+++ src/app/components/settingsModal/SettingsModal.tsx
@@ -1,77 +1,94 @@
-import React, { useState, Fragment } from 'react'
-import IndividualSettingModal from './IndividualSettingModal';
-import ReactDOM from 'react-dom';
-import SettingsControl from './SettingsControl';
-import useModal from '../../hooks/useModal'
-import ModalContainer from '../utilities/modals/ModalContainer';
+import React, { useState, Fragment } from "react";
+import IndividualSettingModal from "./IndividualSettingModal";
+import ReactDOM from "react-dom";
+import SettingsControl from "./SettingsControl";
+import useModal from "../../hooks/useModal";
+import ModalContainer from "../utilities/modals/ModalContainer";
 
-export default function SettingsModal({ toggle: toggleThis }) {
+export default function SettingsModal({ toggle: toggleThis }: any) {
+  // const [currModal, setCurrModal] = useState(null);
+  const { isShowing: open, toggle } = useModal();
 
-	// const [currModal, setCurrModal] = useState(null);
-	const { isShowing: open, toggle } = useModal();
+  // function setModal() {
 
-	// function setModal() {
+  // }
 
-	// }
+  const deactivateText =
+    "Upon deletion, account can be recovered for up to 30 days. After that, all your data will be removed permanently.";
 
-	const deactivateText = "Upon deletion, account can be recovered for up to 30 days. After that, all your data will be removed permanently."
+  const [currentTab, setCurrentTab] = useState("General");
+  function handleClickTab(tabName: any) {
+    setCurrentTab(tabName);
+  }
 
-	const [currentTab, setCurrentTab] = useState("General");
-	function handleClickTab(tabName) { setCurrentTab(tabName) }
+  const tabs = [
+    {
+      title: "General",
+      content: (
+        <Fragment>
+          <SettingsControl
+            title="Email Address"
+            value="dwidmer@visualum.com"
+            subtitle="Managed by Google"
+            linkText="Edit Email"
+            action={toggle}
+          />
+          <SettingsControl
+            title="Full Name"
+            value="Johnny Appleseed"
+            linkText="Edit Name"
+          />
+          <SettingsControl
+            title="Password"
+            subtitle="Last changed: 10/05/21"
+            linkText="Change Password"
+          />
+          <div className="deactivate-account-container">
+            <div className="deactivate-account">
+              <h5 className="title">Deactivate Account</h5>
+              <p className="subtitle">{deactivateText}</p>
+              <div className="w-100 d-flex justify-content-center">
+                <button className="btn btn-danger delete-button">
+                  Delete Account
+                </button>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      ),
+    },
+    { title: "Notifications", content: null },
+    { title: "Accessibility", content: null },
+  ];
 
-	const tabs = [
-		{
-			title: "General",
-			content: <Fragment>
-				<SettingsControl
-					title="Email Address"
-					value="dwidmer@visualum.com"
-					subtitle="Managed by Google"
-					linkText="Edit Email"
-					action={toggle}
-				/>
-				<SettingsControl
-					title="Full Name"
-					value="Johnny Appleseed"
-					linkText="Edit Name"
-				/>
-				<SettingsControl
-					title="Password"
-					subtitle="Last changed: 10/05/21"
-					linkText="Change Password"
-				/>
-				<div className='deactivate-account-container'>
-					<div className='deactivate-account'>
-						<h5 className='title'>Deactivate Account</h5>
-						<p className='subtitle'>{deactivateText}</p>
-						<div className='w-100 d-flex justify-content-center'>
-							<button className='btn btn-danger delete-button'>
-								Delete Account
-							</button>
-						</div>
-					</div>
-				</div>
-			</Fragment>
-		},
-		{ title: "Notifications", content: null },
-		{ title: "Accessibility", content: null }
-	]
-
-	return ReactDOM.createPortal(
-		<ModalContainer title="Settings" toggle={toggleThis} extraClasses={['settings-modal-container-override']}>
-			<div className='settings-modal-container'>
-				<div className='settings-modal'>
-					<div className='body'>
-						<div className='tabs'>
-							{tabs.map(tab => <div className={`tab ${tab.title === currentTab ? 'active' : ""}`} onClick={() => handleClickTab(tab.title)}><div className='tab-text'>{tab.title}</div></div>)}
-						</div>
-						<div className='divider' />
-						<div className='settings-body'>
-							{tabs.filter(tab => tab.title === currentTab)[0].content}
-						</div>
-					</div>
-				</div>
-			</div>
-			{open && <IndividualSettingModal title="Change Email" toggle={toggle} />}
-		</ModalContainer >, document.getElementById('portal'));
+  return ReactDOM.createPortal(
+    <ModalContainer
+      title="Settings"
+      toggle={toggleThis}
+      extraClasses={["settings-modal-container-override"]}
+    >
+      <div className="settings-modal-container">
+        <div className="settings-modal">
+          <div className="body">
+            <div className="tabs">
+              {tabs.map((tab) => (
+                <div
+                  className={`tab ${tab.title === currentTab ? "active" : ""}`}
+                  onClick={() => handleClickTab(tab.title)}
+                >
+                  <div className="tab-text">{tab.title}</div>
+                </div>
+              ))}
+            </div>
+            <div className="divider" />
+            <div className="settings-body">
+              {tabs.filter((tab) => tab.title === currentTab)[0].content}
+            </div>
+          </div>
+        </div>
+      </div>
+      {open && <IndividualSettingModal title="Change Email" toggle={toggle} />}
+    </ModalContainer>,
+	document.body
+  );
 }
